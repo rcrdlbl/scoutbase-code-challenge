6460fcf926941f1fa8e209319d0ed7172c33dad7
rcrdlbl <bluh@outlook.com>
2019-09-03 16:24:35 -0400

    created apollo client instance

--- front-end/src/containers/App.js
+++ front-end/src/containers/App.js
@@ -3,13 +3,27 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+
+const cache = new InMemoryCache()
+const link = new HttpLink({
+  uri: "https://countries.trevorblades.com"
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
 
 class App extends Component {
   render() {
     return(
-      <Router>
-        <Route path="/" render={() => <h1>Root Route</h1>} />
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <Route path="/" render={() => <h1>Root Route</h1>} />
+        </Router>
+      </ApolloProvider>
     )
   }
 }
