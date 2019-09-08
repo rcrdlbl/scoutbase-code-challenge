a46ee3e0a47a70c5a22fac6cff6235d54152b155
rcrdlbl <bluh@outlook.com>
2019-09-08 19:12:16 -0400

    added switches and 404 route/page

---
+++ front-end/src/components/NoMatch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class NoMatch extends Component {
+  render() {
+    return(
+      <h1>♫ 404, I'm rotten to the core ♫</h1>
+    )
+  }
+}
+
+export default NoMatch
--- front-end/src/containers/App.js
+++ front-end/src/containers/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+
+import NoMatch from '../components/NoMatch'
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
@@ -21,8 +24,11 @@ class App extends Component {
     return(
       <ApolloProvider client={client}>
         <Router>
+          <Switch>
           <Route exact path="/" render={() => <h1>Root Route</h1>} />
-          <Route exact path="/countries" render={() => <h1>List of countries</h1>} />
+          <Route path="/countries" render={() => <h1>List of countries</h1>} />
+          <Route component={NoMatch} />
+          </Switch>
         </Router>
       </ApolloProvider>
     )
