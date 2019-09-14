44cf71ab1957897165bf731428e35edd2f167eab
rcrdlbl <bluh@outlook.com>
2019-09-14 18:32:20 -0400

    removed redundant apollo calls

--- front-end/src/containers/CountriesContainer.js
+++ front-end/src/containers/CountriesContainer.js
@@ -1,25 +1,8 @@
 import React, { Component } from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { gql } from 'apollo-boost'
 import styled from 'styled-components'
 
 import CountryListContainer from './CountryListContainer'
 
-
-const COUNTRIES = gql`
-  {
-    countries {
-      code
-      name
-      native
-      continent { name }
-      languages {
-        name
-        native
-      }
-    }
-  }
-`
 
 const listContainer = styled.div`
   margin: 0 auto;
@@ -30,19 +13,6 @@ const navContainer = styled.header`
 `
 
 class CountriesContainer extends Component {
-
-  makeList() {
-    const { loading, error, data } = useQuery(COUNTRIES)
-    if (loading) {
-      return <h3>Loading</h3>
-    } else if (error) {
-      return <h3>Error</h3>
-    } else {
-      console.log(data)
-      return <p>data</p>
-    }
-  }
-
   render() {
     return(
       <listContainer>
